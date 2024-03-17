d7d0d55863a1c18a0f842daad8180bbbb5d957f6
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-17 08:23:03 +0530

    staus reload message removed

--- SafeX-PHP/Backend/construction-site.php
+++ SafeX-PHP/Backend/construction-site.php
@@ -10,21 +10,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['sitename'])) {
         $siteName = $_POST['sitename'];
         $companyID = $_SESSION["Company_ID"];
-
-        // Check if the construction site already exists in the database
         $sql_search_site = "SELECT * FROM construction_site WHERE Site_Name = '$siteName' AND company_id = '$companyID'";
         $result_search_site = $database_connection->query($sql_search_site);
 
         if ($result_search_site->num_rows > 0) {
             $status['message'] = "Site Is Already Added";
         } else {
-            // Site does not exist, insert data into the database
             $insert_site_sql = "INSERT INTO construction_site (Site_Name, company_id) VALUES ('$siteName', '$companyID')";
 
             if ($database_connection->query($insert_site_sql) === TRUE) {
                 $status['status'] = 'success';
                 $status['message'] = "Site Is Added Succesfully";
-                $status['reload'] = true;
             } else {
                 $status['message'] = "Error inserting data: " . $database_connection->error;
             }
