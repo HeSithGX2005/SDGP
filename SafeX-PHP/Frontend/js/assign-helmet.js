3ddf3bcce2c18165ca574ac2351b41089885e2bc
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-17 08:54:13 +0530

    assign-helmet js file created and javascript created to redirect assign.php

---
+++ SafeX-PHP/Frontend/js/assign-helmet.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var assignButtons = document.querySelectorAll('.assign-btn');
+
+    assignButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var userId = this.getAttribute('data-userid');
+            window.location.href = 'assign.php?user_id=' + userId;
+        });
+    });
+});
