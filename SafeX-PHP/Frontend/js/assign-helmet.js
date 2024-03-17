4dade4153c93a99baf21163f7f96191d3440abaf
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-17 10:34:42 +0530

    show field function is added

--- SafeX-PHP/Frontend/js/assign-helmet.js
+++ SafeX-PHP/Frontend/js/assign-helmet.js
@@ -10,3 +10,43 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function showFields() {
+    var selectedOption = document.getElementById("dropdown").value;
+    var dynamicForm = document.getElementById("dynamicForm");
+    
+    dynamicForm.innerHTML = "";
+    
+    if (selectedOption === "Automatic") {
+        dynamicForm.innerHTML += '<label for="NumofWorker">Number of Worker:</label>';
+        dynamicForm.innerHTML += '<input type="number" id="input1" name="NumofWorker" required>';
+        dynamicForm.innerHTML += '<label for="NumofSupervisior">Number of Supervisior:</label>';
+        dynamicForm.innerHTML += '<input type="number" id="input1" name="NumofSupervisior" required>';
+        dynamicForm.innerHTML += '<label for="SiteName">Site Name:</label>';
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                dynamicForm.innerHTML += xhr.responseText;
+            }
+        };
+        xhr.open("GET", "dropdown_ConstructionSite.php", true);
+        xhr.send();
+
+        
+        dynamicForm.innerHTML += '<input type="submit" value="Assign Worker">';
+    } else if (selectedOption === "Manual") {
+        var dropdownHTML = '<label for="position">Position: </label>';
+        dropdownHTML += '<select id="position" name="position" required>';
+        dropdownHTML += '<option value="worker">Construction Worker</option>';
+        dropdownHTML += '<option value="supervisor">Supervisor</option>';
+        dropdownHTML += '</select>';
+        
+        // Append the dropdown menu HTML to dynamicForm
+        dynamicForm.innerHTML += dropdownHTML;;
+        dynamicForm.innerHTML += '<label for="employeeSearch">Search Employee:</label>';
+        dynamicForm.innerHTML += '<input type="text" id="employeeSearch" name="employeeSearch" placeholder="Search..." required>';
+        dynamicForm.innerHTML += '<button type="button" onclick="searchEmployee()">Search</button>';
+        
+    } 
+    
+}
