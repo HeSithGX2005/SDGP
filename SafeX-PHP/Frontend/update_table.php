15e9d02589a2320d007644576d7cdd7270f49199
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-18 11:10:18 +0530

    Dynamic Button Is Added

--- SafeX-PHP/Frontend/update_table.php
+++ SafeX-PHP/Frontend/update_table.php
@@ -4,7 +4,7 @@ require "../Backend/database.php";
 $companyID = $_SESSION["Company_ID"];
 $siteId = $_POST['siteId'];
 $selectedOption = $_POST['selectedOption'];
-
+$row;
 if ($selectedOption === "Automatic") {
     // Query for automatic assignment
     $assigned_workers_query = "SELECT e.Name, e.Position, saw.Site_ID, saw.Employee_ID 
@@ -12,13 +12,12 @@ if ($selectedOption === "Automatic") {
                                 JOIN employee e ON saw.Employee_ID = e.Employee_Id
                                 WHERE saw.Site_ID = '$siteId'
                                 ORDER BY e.Name";
-    $button = '<a href="assign-helmet.php?site_ID='.$siteId.'delete=' . $row['Employee_ID'] . '" class="btn btn-danger">Delete</a>';
 } elseif($selectedOption === "Manual") {
     $assigned_workers_query = "SELECT  * FROM employee WHERE Company_ID = '$companyID' AND Assigned =0  ";
-   
 }
 
 $assigned_workers_result = $database_connection->query($assigned_workers_query);
+
 
 $row;
 $row_number = 1;
@@ -27,6 +26,11 @@ $table_html = '';
 $assigned_workers_result = mysqli_query($database_connection, $assigned_workers_query);
 if ($assigned_workers_result->num_rows > 0) {
     while ($row = $assigned_workers_result->fetch_assoc()) {
+        if ($selectedOption === "Automatic") {
+            $button='<a href="assign-helmet.php?site_ID='.$siteId.'delete=' . $row['Employee_ID'] . '" class="btn btn-danger">Delete</a>';
+        } elseif($selectedOption === "Manual") {
+            $button='<a href="assign-helmet.php?site_ID='.$siteId.'update=' . $row['Employee_ID'] . '" class="btn btn-danger">Assigned Worker</a>';
+        }
         echo '<tr>
         <td>' . $row_number . '</td>
         <td>' . $row['Name'] . '</td>
