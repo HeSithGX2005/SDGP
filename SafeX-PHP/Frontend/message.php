3deb83a677c0824e542923606a844ecfcb9f9f21
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-18 21:53:57 +0530

    Sweet Alert JS Embeded

--- SafeX-PHP/Frontend/message.php
+++ SafeX-PHP/Frontend/message.php
@@ -21,28 +21,34 @@ require ("sidepanel.php");
                     <div class="col-md-6 mt-5">
                         <div class="card">
                             <div class="card-body">
-                                <form method="post" action="../Backend/message.php">
+                                <form method="post" action="../Backend/message.php" id="message-form">
                                     <div class="row">
                                     <div class="col">
                                         <input type="text" class="form-control" placeholder="To" name="receiver">
                                     </div>
                                     <div class="col">
-                                        <input type="hidden" class="form-control" placeholder="From" value="<?php if(isset($_SESSION["Company_ID"])){
+                                        <input type="hidden" class="form-control"  name="from" value="<?php if(isset($_SESSION["Company_ID"])){
                                             $companyID = $_SESSION["Company_ID"];echo $companyID;
                                         }elseif(isset( $_SESSION["Employee_ID"])){$employeeID = $_SESSION["Employee_ID"];echo $employeeID;}
                                         ?>">
                                     </div>
                                     </div><br>
                                     <div class="row">
-                                        <textarea class="form-control" rows="5" id="message" placeholder="Message" name="text"></textarea>
+                                        <textarea class="form-control" rows="5" id="message" placeholder="Message" name="message-body" required></textarea>
+                                    </div>
+                                    <div class="row">
+                                        <input type="submit" class="btn btn-primary" value="Sent">
                                     </div>
                                 </form><br>
                             </div>
                         </div><br>
-                        <button type="button" class="btn btn-primary">Sent</button>
                     </div>
                 </div>
             </div>
         </div>
+        <script src="js/notification-panel.js"></script>
+    <script>
+        handleFormSubmit("message-form");
+    </script>
 </body>
 </html>
