6d6e4052b85434e1a8ad6687c0d167ac1dfe4777
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-18 13:15:17 +0530

    Report Isssueing Fronend Code and Dynamic Code are Created

---
+++ SafeX-PHP/Frontend/report.php
@@ -0,0 +1,52 @@
+<?php 
+require("../Backend/database.php");
+include("css/css-links.php");
+require ("sidepanel.php");
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SafeX|Report</title>
+    <link rel="stylesheet" type="text/css" href="css/message.css">
+    <style>
+        #report{
+            color: black;
+        }
+    </style>
+</head>
+<body>
+        <!--main content-->
+        <div class="content text-center mt-5">
+            <div class="container">
+
+                <div class="row justify-content-center">
+                    <div class="col-md-6 mt-5">
+                        <h1 id="report">Report Isssue</h1>
+                        <div class="card">
+                            <div class="card-body">
+                                <form>
+                                    <div class="row">
+                                        <input type="hidden" class="form-control" placeholder="Name" name="name" value=<?php if(isset($_SESSION["Company_ID"])){
+                                            $companyID = $_SESSION["Company_ID"];echo $companyID;
+                                        }elseif(isset( $_SESSION["Employee_ID"])){$employeeID = $_SESSION["Employee_ID"];echo $employeeID;}
+                                        ?>>
+                                    <br>
+                                    <div class="row">
+                                        <textarea class="form-control" rows="5" id="message" placeholder="Issue" name="text"></textarea>
+                                    </div>
+                                </form><br>
+                            </div>
+                        </div><br>
+                        <button type="button" class="btn btn-primary">Sent</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
