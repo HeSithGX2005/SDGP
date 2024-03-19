a0d59589cb613a6069e0636d392ae73a37a9c53a
Nipuna Kaweya <nipuna609@gmail.com>
2024-03-19 19:28:34 +0530

    Message All Fixes

--- SafeX-PHP/Backend/message.php
+++ SafeX-PHP/Backend/message.php
@@ -4,17 +4,20 @@ require "database.php";
 
 if (!isset($_SESSION["Company_ID"])) {
     $_SESSION["Company_ID"] = 0;
-}else{
-    $companyID =    $_SESSION["Company_ID"];
+    $companyID = 0;
+} else {
+    $companyID = $_SESSION["Company_ID"];
 }
 
 if (!isset($_SESSION["Employee_ID"])) {
     $_SESSION["Employee_ID"] = 0;
-}else{
-    $employeeID =    $_SESSION["Employee_ID"];
+    $employeeID = 0;
+} else {
+    $employeeID = $_SESSION["Employee_ID"];
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    echo "Received1";
     $status = array();
     $status['status'] = 'error';
     $status['message'] = '';
@@ -23,7 +26,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $receiver=$_POST['receiver'];
         $from = $_POST['from'];
         $message = $_POST['message-body'];
-
+        echo "Received2";
         if($from == $companyID){
             $selectEmployeeSql = "SELECT * FROM employee WHERE Company_ID = $companyID AND Name ='$receiver' AND Position = 'supervisor'";
             $resultEmployee = $database_connection->query($selectEmployeeSql);
@@ -38,6 +41,55 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $insertMessageSql = "INSERT INTO messaging(Sender_Company_ID,Receiver_Employee_ID, Message_Body) VALUES ('$from', '$receiverID', '$message')";
                 $resultMessage = $database_connection->query($insertMessageSql);
                 if ($resultMessage == false) {
+                    //$status['message'] = "Error executing query: " . $database_connection->error;;
+                    //echo json_encode($status);
+                    echo "Error executing query: " . $database_connection->error;
+                    exit();
+                }else{
+                    echo "Message Sent Succefully";
+                    //$status['status'] = 'success';
+                    //$status['message'] = 'Message Sent Successfully';
+                    //echo json_encode($status);
+                    exit();
+                }
+            }
+        }else if ($from == $employeeID){
+            echo "Received";
+            $selectWorkerCompanySql = "SELECT e.*, c.Company_Name FROM  employee e JOIN company c ON e.Company_ID = c.Company_ID";
+            $resultWorkerCompany = $database_connection -> query($selectWorkerCompanySql);
+            if($resultWorkerCompany == true){
+                while($rowEmployee =$resultWorkerCompany-> fetch_assoc()){
+                    $receiverID = $rowEmployee['Employee_ID'];
+                    $insertMessageSql = "INSERT INTO messaging(Sender_Employee_ID,Receiver_Company_ID, Message_Body) VALUES ('$from', '$receiverID', '$message')";
+                    $resultMessage = $database_connection->query($insertMessageSql);
+                if ($resultMessage == false) {
+                    //$status['message'] = "Error executing query: " . $database_connection->error;;
+                    //echo json_encode($status);
+                    echo "Error executing query: " . $database_connection->error;
+                    exit();
+                }else{
+                    echo "Message Sent Succefully";
+                    //$status['status'] = 'success';
+                    //$status['message'] = 'Message Sent Successfully';
+                    //echo json_encode($status);
+                    exit();
+                }
+                }
+            }
+
+            }else{
+                echo "No Company Message";
+                $selectWorkerSql="SELECT * FROM employee WHERE Company_ID = $companyID AND Name ='$receiver' AND Position = 'supervisor'";
+                $resultWorker = $database_connection -> query($selectWorkerSql);
+                if($resultWorker === false){
+                    echo "Error executing query: " . $database_connection->error;
+                    exit();
+                }
+                while($rowEmployee =$resultWorker-> fetch_assoc()){
+                    $receiverID = $rowEmployee['Employee_ID'];
+                    $insertMessageSql = "INSERT INTO messaging(Sender_Employee_ID,Receiver_Employee_ID, Message_Body) VALUES ('$from', '$receiverID', '$message')";
+                    $resultMessage = $database_connection->query($insertMessageSql);
+                if ($resultMessage == false) {
                     $status['message'] = "Error executing query: " . $database_connection->error;;
                     echo json_encode($status);
                     //echo "Error executing query: " . $database_connection->error;
@@ -49,12 +101,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     echo json_encode($status);
                     exit();
                 }
+                }
             }
         }
         
     }
 
-}
 
 
 ?>
